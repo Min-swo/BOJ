dd635bde64eaca89f0719f7aaff25018fdacf162
Minseo Kim <80453129+Min-swo@users.noreply.github.com>
2024-03-20 18:15:11 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int k;
+	cin >> k;
+	k = k / 4;
+	for(int i = 0; i < k; i++)
+		cout << "long ";
+	
+	cout << "int";
+	
+	
+	return 0;
+}
