579b9001bd6c8c520701c54c041340a771655042
Minseo Kim <80453129+Min-swo@users.noreply.github.com>
2024-03-20 18:06:21 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int yr;
+    cin >> yr;
+    
+    if(yr % 4 == 0 && (yr % 100 != 0 || yr % 400 == 0))
+        cout << 1;
+    else
+        cout << 0;
+ 
+    
+    
+}
