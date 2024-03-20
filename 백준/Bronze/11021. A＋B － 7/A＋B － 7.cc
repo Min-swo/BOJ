fc39557e604600158501865c707f09a4878db08c
Minseo Kim <80453129+Min-swo@users.noreply.github.com>
2024-03-20 18:11:35 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	
+	int k;
+	cin >> k;
+	
+	int a, b;
+	for(int i = 0; i < k; i++)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i+1 << ": " << a + b << "\n";
+	}
+	
+	return 0;
+}
