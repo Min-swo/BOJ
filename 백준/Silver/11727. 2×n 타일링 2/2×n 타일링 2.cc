6f05ae094c7d29062779dfaef3ddd83db9b96dc4
Minseo Kim <80453129+Min-swo@users.noreply.github.com>
2024-03-20 18:12:52 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 1001
+
+int main(void)
+{
+    int n;
+    cin >> n;
+
+    int method[MAX];
+
+    method[1] = 1;
+    method[2] = 3;
+    for(int i = 3; i <= n; i++)
+        method[i] = (method[i-1] + method[i-2] * 2) % 10007;
+    cout << method[n] << "\n";
+}
