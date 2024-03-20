5ce2109a480defe18763993796cb908961b664d6
Minseo Kim <80453129+Min-swo@users.noreply.github.com>
2024-03-20 18:12:21 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+
+int main(void)
+{
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	
+	char ch;
+	cin >> ch;
+	
+	cout << int(ch);
+	
+	return 0;
+}
