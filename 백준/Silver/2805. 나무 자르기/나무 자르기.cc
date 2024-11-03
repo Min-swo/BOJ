50a92c1d707cd5f9ae1af1e357b34f3731b8f17d
Minseo Kim <80453129+Min-swo@users.noreply.github.com>
2024-11-03 16:05:15 +0900

    [Silver II] Title: 나무 자르기, Time: 620 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long N, M;
+long height = 0;
+long targetIdx = -1;
+long ans = 0;
+vector<long> treeHeights;
+
+bool compare(long a, long b) { return a > b; }
+
+int main(void) {
+    cin >> N >> M;
+    for (long i = 0; i < N; i++) {
+        long tmpHeight;
+        cin >> tmpHeight;
+        treeHeights.push_back(tmpHeight);
+    }
+    treeHeights.push_back(0);
+    N += 1;
+
+    sort(treeHeights.begin(), treeHeights.end(), compare);
+
+    for (long i = 1; true; i++) {
+        if (height >= M) {
+            targetIdx = i - 1;
+            break;
+        }
+        height += (treeHeights[i - 1] - treeHeights[i]) * i;
+    }
+
+    if (height == M) {
+        ans = treeHeights[targetIdx];
+    } else {
+        ans = treeHeights[targetIdx] + (height - M) / targetIdx;
+    }
+    cout << ans;
+    return 0;
+}
