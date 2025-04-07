0e6e620bf0784e4e1d6816e15b4890ca7aefe022
Minseo Kim <80453129+Min-swo@users.noreply.github.com>
2025-04-07 14:44:16 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-#define MAX 15
+#define MAX 20
 int prices[MAX];
 int times[MAX];
 int dp[MAX];
@@ -15,30 +15,20 @@ int main(void) {
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> times[i] >> prices[i];
-        if (i + times[i] <= N) {
-            dp[i] = prices[i];
-        } else {
-            dp[i] = 0;
-        }
+        dp[i] = 0;
     }
+    dp[N] = 0;
 
-    int maxValue = dp[0];
-    for (int i = 1; i < N; i++) {
-        for (int j = i - 1; j >= 0; j--) {
-            if (times[j] + j <= i) {
-                int tmp = 0;
-                if (i + times[i] <= N) {
-                    tmp = dp[j] + prices[i];
-                } else {
-                    tmp = dp[j];
-                }
-                dp[i] = max(dp[i], tmp);
-            }
+    int maxValue = 0;
+    for (int i = N - 1; i >= 0; i--) {
+        if (i + times[i] <= N) {
+            dp[i] = max(prices[i] + dp[i + times[i]], dp[i + 1]);
+        } else {
+            dp[i] = dp[i + 1];
         }
         maxValue = max(maxValue, dp[i]);
     }
 
     cout << maxValue << "\n";
-
     return 0;
 }
