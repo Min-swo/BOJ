8bada9c2f5d76ef9acdd91b6939f5c93871c950d
Minseo Kim <80453129+Min-swo@users.noreply.github.com>
2025-04-07 10:54:42 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,43 +1,34 @@
+#include <algorithm>
 #include <iostream>
-#include <queue>
 #include <vector>
 using namespace std;
 
 #define MAX 10001
 int V, E;
-vector<pair<int, int>> v[MAX];
-int visited[MAX];
+vector<pair<int, pair<int, int>>> v;
+int parent[MAX];
 
-int prim(int start) {
-    priority_queue<pair<int, int>> q;
+int findParent(int node) {
+    if (parent[node] != node) {
+        parent[node] = findParent(parent[node]);
+    }
+    return parent[node];
+}
+
+int kruskal() {
     int cnt = 0;
     int sum = 0;
+    for (pair<int, pair<int, int>> e : v) {
+        int weight = e.first;
+        int start = e.second.first;
+        int end = e.second.second;
 
-    visited[start] = 1;
-    for (pair<int, int> e : v[start]) {
-        q.push({-1 * e.first, e.second});
-    }
-
-    while (!q.empty() && cnt < V - 1) {
-        int cur = q.top().second;
-        int curW = q.top().first;
-        q.pop();
-
-        if (visited[cur]) {
-            continue;
-        }
-
-        sum += -1 * curW;
-        cnt++;
-
-        visited[cur] = 1;
-
-        for (pair<int, int> e : v[cur]) {
-            int next = e.second;
-            int nextW = e.first;
-            if (!visited[next]) {
-                q.push({-1 * nextW, next});
-            }
+        int startParent = findParent(start);
+        int endParent = findParent(end);
+        if (endParent != startParent) {
+            parent[endParent] = startParent;
+            cnt++;
+            sum += weight;
         }
     }
 
@@ -51,16 +42,17 @@ int main(void) {
 
     cin >> V >> E;
     for (int i = 1; i <= V; i++) {
-        visited[i] = 0;
+        parent[i] = i;
     }
 
     int a, b, w;
     for (int i = 0; i < E; i++) {
         cin >> a >> b >> w;
-        v[a].push_back({w, b});
-        v[b].push_back({w, a});
+        v.push_back({w, {a, b}});
     }
 
-    cout << prim(1) << "\n";
+    sort(v.begin(), v.end());
+    cout << kruskal() << "\n";
+
     return 0;
 }
