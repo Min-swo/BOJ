c063bc192653fdaa49e05a48ad858e79f2e28a7b
Minseo Kim <80453129+Min-swo@users.noreply.github.com>
2025-04-11 17:55:28 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,30 +1,26 @@
-WITH RECURSIVE GenerationTree AS (
-    -- 1세대: 최초 개체 (PARENT_ID가 NULL)    
+WITH RECURSIVE rc AS (
     SELECT 
         ID,
-        CASE
-            WHEN NOT EXISTS (select 1 from ECOLI_DATA e where e.PARENT_ID = e2.ID) THEN 1
-            ELSE 0 
-        END AS NO_CHILD,
+        PARENT_ID,
         1 AS GENERATION
-    FROM ECOLI_DATA e2
+    FROM ECOLI_DATA
     WHERE PARENT_ID IS NULL
 
     UNION ALL
 
-    -- 2세대 이상: 부모의 GENERATION + 1
     SELECT 
-        e3.ID,
-        CASE
-            WHEN NOT EXISTS (select 1 from ECOLI_DATA e4 where e4.PARENT_ID = e3.ID) THEN 1
-            ELSE 0 
-        END AS NO_CHILD,
-        gt.GENERATION + 1
-    FROM ECOLI_DATA e3 JOIN GenerationTree gt 
-        ON e3.PARENT_ID = gt.ID and gt.NO_CHILD = 0
+        e.ID,
+        e.PARENT_ID,
+        rc.GENERATION + 1
+    FROM ECOLI_DATA e
+    JOIN rc ON e.PARENT_ID = rc.ID
 )
 
-SELECT SUM(NO_CHILD) AS COUNT, GENERATION
-    FROM GenerationTree
-        GROUP BY GENERATION
-ORDER BY GENERATION;
+SELECT COUNT(ID) AS COUNT, GENERATION FROM rc r
+    WHERE NOT EXISTS (
+                SELECT 1 
+                FROM rc c 
+                WHERE c.PARENT_ID = r.ID
+            )
+    GROUP BY GENERATION
+    ORDER BY GENERATION;
