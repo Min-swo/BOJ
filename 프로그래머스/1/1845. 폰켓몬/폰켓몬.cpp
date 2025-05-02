da7768ed7d6c147ba975cbc7a2f741b8db01d1ce
Minseo Kim <80453129+Min-swo@users.noreply.github.com>
2025-05-02 18:35:49 +0900

    [level 1] Title: 폰켓몬, Time: 0.90 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,33 +1,10 @@
 #include <vector>
+#include <unordered_set>
+
 using namespace std;
 
 int solution(vector<int> nums)
 {
-    int N = nums.size();
-    int type[200001];
-    
-    for(int i = 0; i < 200001; i++) {
-        type[i] = 0;
-    }
-    
-    for (int i : nums) {
-        type[i] = 1;
-    }
-    
-    int cnt = 0;
-    for(int i = 1; i < 200001; i++) {
-        if(type[i] == 1) {
-            cnt++;
-        }
-    }
-    
-    int answer = 0;
-    
-    if(cnt >= N / 2) {
-        answer = N / 2;
-    } else {
-        answer = cnt;
-    }
-    
-    return answer;
+    unordered_set<int> s(nums.begin(), nums.end());
+    return min(nums.size() / 2, s.size());
 }
