c74cace1f595c4f5746f9028b3353b05b65c08a8
Minseo Kim <80453129+Min-swo@users.noreply.github.com>
2025-06-19 14:10:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.66 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,21 +1,16 @@
 function solution(participant, completion) {
-    let answer = '';
-    
     const map = {};
     for(const name of participant) {
-        map[name] = 0;
+        map[name] = (map[name] ?? 0) + 1;
     }
-    for(const name of participant) {
-        map[name]++;
-    }
+   
     for(const name of completion) {
         map[name]--;
     }
     
     for(const key in map) {
         if(map[key] != 0) {
-            answer = key;
+            return key;
         }
     }
-    return answer;
 }
