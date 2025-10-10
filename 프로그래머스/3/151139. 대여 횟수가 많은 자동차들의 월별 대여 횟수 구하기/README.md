48dd77fdb6699210f0bbda095766905a39cdd711
Minseo Kim <80453129+Min-swo@users.noreply.github.com>
2025-10-10 17:03:25 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/README.md
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기 - 151139 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151139) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151139#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 05월 01일 13:29:09
+2025년 10월 10일 17:03:24
 
 ### 문제 설명
 
--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,15 +1,18 @@
--- 코드를 입력하세요
-SELECT MONTH(START_DATE) AS MONTH, CAR_ID, COUNT(*) AS RECORDS
-    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-        WHERE CAR_ID IN (SELECT CAR_ID
-                        FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-                            WHERE YEAR(START_DATE) = 2022 
-                                AND MONTH(START_DATE) BETWEEN 8 AND 10
-                            GROUP BY CAR_ID
-                            HAVING COUNT(CAR_ID) >= 5)
-            AND YEAR(START_DATE) = 2022 
-            AND MONTH(START_DATE) BETWEEN 8 AND 10
-            GROUP BY CAR_ID, MONTH(START_DATE)
-                ORDER BY MONTH, CAR_ID DESC
-                
-         
+WITH TARGET_CAR AS (
+    SELECT CAR_ID, COUNT(CAR_ID) AS RENT_COUNT FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+        WHERE START_DATE >= '2022-08-01'
+        AND START_DATE <= '2022-10-31'
+            GROUP BY CAR_ID
+)
+
+SELECT MONTH(CR.START_DATE) AS `MONTH`,
+        CR.CAR_ID, 
+        COUNT(*) AS RECORDS 
+            FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY CR
+                    INNER JOIN TARGET_CAR TC
+                    ON CR.CAR_ID = TC.CAR_ID
+                        WHERE TC.RENT_COUNT >= 5
+                        AND CR.START_DATE >= '2022-08-01'
+                        AND CR.START_DATE <= '2022-10-31'
+                            GROUP BY MONTH(CR.START_DATE), CR.CAR_ID
+                                ORDER BY MONTH(CR.START_DATE), CR.CAR_ID DESC
